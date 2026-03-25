d7a84db30cc7c63b7bfd02a153f9977524eddf92
AndrewTobias1973 <a.j.tobias@tcu.edu>
2026-03-25 15:50:14 -0500

    Refactor Streamlit app by reordering imports

--- Portforlio/StreamlitApp_Week_7.py
+++ Portforlio/StreamlitApp_Week_7.py
@@ -59,7 +59,7 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
-        "keys": ["APPL", "MPWR"],
+        "keys": ["AMD", "NVDA"],
         "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["AMD", "NVDA"]]
 }
 
@@ -159,6 +159,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
