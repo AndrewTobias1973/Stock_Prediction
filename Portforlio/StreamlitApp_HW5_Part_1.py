42d60d72e49d8f9432f120f1f6186fd68a404e19
AndrewTobias1973 <a.j.tobias@tcu.edu>
2026-04-07 16:18:20 -0500

    Refactor Streamlit app for model prediction

--- Portforlio/StreamlitApp_HW5_Part_1.py
+++ Portforlio/StreamlitApp_HW5_Part_1.py
@@ -59,8 +59,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pca.shap',
         "pipeline": 'finalized_model.tar.gz',
-        "keys": ["AMD"],
-        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 100.0, "step": 10.0} for k in ["AMD"]]
+        "keys": ["AOS_CR_Cum","AFL_CR_Cum],
+        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 100.0, "step": 10.0} for k in ["MSFT"]]
 }
 
 def load_pipeline(_session, bucket, key):
@@ -113,7 +113,7 @@ def display_explanation(input_df, session, aws_bucket):
     explainer = load_shap_explainer(session, aws_bucket, posixpath.join('explainer', explainer_name),os.path.join(tempfile.gettempdir(), explainer_name))
 
     dataset = pd.read_csv(r'./SP500Data.csv',index_col=0)
-    random = 'AMD'
+    random = 'MSFT'
     random_price = json.loads(request_body)[random]
     closest_date = (dataset[random] - float(random_price)).abs().idxmin()
 
